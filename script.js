8ee72f654f4fe909b78195fcad378f97d0b120a7
ABDULREHMAN <rehmanmeil074@gmail.com>
2026-02-15 00:08:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const professions = ['Data Scientist', 'ML & AI Practioner', 'Focused on LLMs | 
 let professionIndex = 0;
 let charIndex = 0;
 let isDeleting = false;
-const typingSpeed = 99;
+const typingSpeed = 98;
 const deletingSpeed = 55;
 const pauseTime = 2000;
 
@@ -123,5 +123,6 @@ const skillTags = document.querySelectorAll('.skill-tag');
 skillTags.forEach((tag, index) => {
     tag.style.setProperty('--delay', index);
 });
+
 
 
